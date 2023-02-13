c2f9bbaa3a72f6054b0758166eb19dbf478f2701
EkaterinaRus <77588157+EkaterinaRus@users.noreply.github.com>
2023-02-13 16:37:52 +0300

    Delete C5_5_2 directory

--- C5_5_2/C6_5_2.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.j-btn-test');
-
-btn.addEventListener('click', () => {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    const message = `Ширина вашего экрана ${width}. Высота вашего экрана ${height}`
-    window.alert(message)
-});
